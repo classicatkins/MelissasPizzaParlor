aca994ec15598f582f5a66b70596bec0029455c9
Melissa Atkins <classicatkins@gmail.com>
2023-12-27 21:19:47 -0600

    8.2

--- server/app.js
+++ server/app.js
@@ -1,11 +1,87 @@
 import express from "express";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
 
 const app = express();
+
+const PORT = process.env.PORT || 4040;
+
+// CORS Middleware
+const cors = (req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+};
+
+// Logging Middleware
+const logging = (request, response, next) => {
+  console.log(
+    `${request.method} ${request.url} ${new Date().toLocaleString("en-us")}`
+  );
+  next();
+};
+
+app.use(cors);
+app.use(logging);
+
+// NOTE: MIDDLEWARE GOES BEFORE THE CREATION OF THE ROUTES :)
+
 // Request handlers go here
 app.get("/status", (request, response) => {
-  response.send(JSON.stringify({ message: "Service healthy" }));
+  response.status(200).json({ message: "Service healthy" });
 });
-app.listen(4040, () => console.log("Listening on port 4040"));
+
+app.get("/weather/:city", (request, response) => {
+  // Express adds a "params" Object to requests that has an matches parameter created using the colon syntax
+  const city = request.params.city;
+
+  // Set defaults values for the query string parameters
+  let cloudy = "clear";
+  let rainy = false;
+  let lowTemp = 32;
+  // check if the request.query.cloudy attribute exists
+  if ("cloudy" in request.query) {
+    // If so update the variable with the query string value
+    cloudy = request.query.cloudy;
+  }
+  if ("rainy" in request.query && request.query.rainy === "true") {
+    rainy = request.query.rainy;
+  }
+  if ("lowtemp" in request.query) {
+    lowTemp = Number(request.query.lowtemp);
+  }
+  // Generate a random number to use as the temperature
+  // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values_inclusive
+  const min = 70;
+  const max = 90;
+  const temp = Math.floor(Math.random() * (max - min + 1) + min);
+  // handle GET request for weather with an route parameter of "city"
+  response.json({
+    text: `The weather in ${city} is ${temp} degrees today.`,
+    cloudy: cloudy,
+    // When the key and value variable are named the same you can omit the value variable
+    rainy,
+    temp: {
+      current: temp,
+      low: lowTemp
+    },
+    city
+  });
+});
+
+app.listen(PORT, () => console.log("Listening on port 4040"));
+
+// BASIC HTTP SERVER BELOW
 
 // // 'Import' the http module
 // import http from "http";
